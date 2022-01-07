02defb4eb774aabe8405771f4bd32cbb89bbb6d3
Philipp Lackner <mail@pl-coding.com>
2022-01-07 19:10:50 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+o
+?app/src/test/java/com/plcoding/calorytracker/ExampleUnitTest.kt,1\f\1f18982e1f7b9f7f55ab86e784ab8b2ae6bb775e
--- app/src/main/java/com/mouhsinbourqaiba/caloryapp/MainActivity.kt
+++ app/src/main/java/com/mouhsinbourqaiba/caloryapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.mouhsinbourqaiba.caloryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mouhsinbourqaiba.caloryapp.ui.theme.CaloryTrackerTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CaloryTrackerTheme {
+
+            }
+        }
     }
 }
