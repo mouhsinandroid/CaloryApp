7c470c5441e03a384408ed41e04215f59e9b9954
mouhsinandroid <moushin.bourqaiba.partner@decathlon.com>
2022-02-18 14:08:50 +0100

    feat - home navigation

--- onboarding/onboarding_presentation/src/main/java/com/mouhsinbourqaiba/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/mouhsinbourqaiba/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -9,12 +9,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.mouhsinbourqaiba.core.R
+import com.mouhsinbourqaiba.core.navigation.Route
+import com.mouhsinbourqaiba.core.util.UiEvent
 import com.mouhsinbourqaiba.core_ui.localSpacing
 import com.mouhsinbourqaiba.onboarding_presentation.components.ActionButton
 
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = localSpacing.current
     Column(
         modifier =  Modifier.fillMaxSize()
@@ -30,7 +34,7 @@ fun WelcomeScreen() {
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = { /*TODO*/ },
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
